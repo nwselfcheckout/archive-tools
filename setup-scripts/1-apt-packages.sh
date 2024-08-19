7afa5a268aee4d93c05824026c61f787efa61bea
mosguinz <mos.guinz@gmail.com>
2024-08-18 22:00:51 -0700

    Update JDK

--- setup-scripts/1-apt-packages.sh
+++ setup-scripts/1-apt-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install screen openjdk-17-jre-headless firewalld pip
+sudo apt install screen openjdk-21-jre firewalld pip
 
 echo Checking to see if everything\'s installed...
 
