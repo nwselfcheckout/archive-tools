1d9fa7a43fbb65a45571cd38f8b50313cddf1e48
mosguinz <mos.guinz@gmail.com>
2024-08-20 18:21:00 -0700

    Add zip and unzip to install

--- setup-scripts/1-apt-packages.sh
+++ setup-scripts/1-apt-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install screen openjdk-21-jre firewalld pip
+sudo apt install screen openjdk-21-jre firewalld pip zip unzip
 
 echo Checking to see if everything\'s installed...
 
