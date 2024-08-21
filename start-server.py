37412d55d81f1a5ff243e6ad665cbd525afd63fe
mosguinz <mos.guinz@gmail.com>
2024-08-21 16:22:40 -0700

    Update launch options

--- start-server.py
+++ start-server.py
@@ -10,7 +10,7 @@ import schedule
 from mcrcon import MCRcon
 
 SERVER_FOLDER = "."
-LAUNCH_ARGS = "-Xms2G -Xmx10G -XX:+UseG1GC"
+LAUNCH_ARGS = "-Xms16G -Xmx16G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1"
 
 START_TIME = time.time()
 
@@ -39,9 +39,11 @@ def get_launch_args(folder):
 def start_server():
     """Open a new terminal window and start the server."""
     launch_args = get_launch_args(SERVER_FOLDER)
-    cmd = (f"screen -S mc_server -dm bash -c"
-           f" \"cd '{os.path.abspath(SERVER_FOLDER)}';"
-           f" java {launch_args} -jar server.jar nogui\"")
+    cmd = (
+        f"screen -S mc_server -dm bash -c"
+        f" \"cd '{os.path.abspath(SERVER_FOLDER)}';"
+        f' java {launch_args} -jar server.jar nogui"'
+    )
 
     print()
     print("> " + cmd)
@@ -50,23 +52,27 @@ def start_server():
 
 
 def send_command(cmd):
-    with MCRcon(host="localhost", password=RCON_PASSWORD, port=RCON_PORT, timeout=10) as mc:
+    with MCRcon(
+        host="localhost", password=RCON_PASSWORD, port=RCON_PORT, timeout=10
+    ) as mc:
         print("[CONSOLE] " + mc.command(cmd))
 
 
 def warn_server(color, time):
     """Send a message to warn players that the server is restarting."""
-    tellraw_arg = ["", {"text": "⚠ ", "color": color},
-                   {"text": "Warning    ", "bold": True, "color": color},
-                   {"text": f"The server will restart in {time}.", "color": color}]
+    tellraw_arg = [
+        "",
+        {"text": "⚠ ", "color": color},
+        {"text": "Warning    ", "bold": True, "color": color},
+        {"text": f"The server will restart in {time}.", "color": color},
+    ]
 
     send_command("tellraw @a " + json.dumps(tellraw_arg))
     print(f"Sent a warning that the server will restart in {time}.")
 
 
 def stop_and_start_server():
-    send_command(
-        "kick @a The server is restarting. Service will resume momentarily.")
+    send_command("kick @a The server is restarting. Service will resume momentarily.")
     send_command("stop")
     print("Sent a request to stop the server. Will relaunch in 60 seconds.")
     time.sleep(60)
