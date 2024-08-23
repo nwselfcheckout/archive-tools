1b4e0875e168e75f7792eee10693bc920cf5a31d
mosguinz <mos.guinz@gmail.com>
2024-08-22 21:11:31 -0700

    Update embed style for coordinates

--- webhooks/run_webhooks.py
+++ webhooks/run_webhooks.py
@@ -13,11 +13,6 @@ WEBHOOK_URL = os.environ["WEBHOOK_URL"]
 def coord_embed(entry: CoordinateEntry) -> Embed:
     embed = Embed()
     embed.title = entry.comment[:256] if entry.comment else "*No label*"
-    embed.set_author(
-        name=entry.username,
-        icon_url=f"https://mc-heads.net/avatar/{entry.username}",
-        url="https://github.com/nwselfcheckout/mc-utils",
-    )
     embed.add_field(
         name="X", value=f"```{'-' if entry.x is None else entry.x}```", inline=True
     )
@@ -27,7 +22,9 @@ def coord_embed(entry: CoordinateEntry) -> Embed:
     embed.add_field(
         name="Z", value=f"```{'-' if entry.z is None else entry.z}```", inline=True
     )
-    embed.set_footer(text=entry.username)
+    embed.set_footer(
+        text=entry.username, icon_url=f"https://mc-heads.net/avatar/{entry.username}"
+    )
     embed.timestamp = entry.dt
     return embed
 
@@ -45,5 +42,5 @@ if __name__ == "__main__":
         raise ValueError("No log folder provided.")
 
     while True:
-        time.sleep(POLLING_INTERVAL)
         send_coords(sys.argv[1])
+        time.sleep(POLLING_INTERVAL)
