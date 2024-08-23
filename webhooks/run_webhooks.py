01054efd1e451ccc6075a3f1501525b52e1d9f9d
mosguinz <mos.guinz@gmail.com>
2024-08-23 15:23:59 -0700

    Add webhook for sending server status

--- webhooks/run_webhooks.py
+++ webhooks/run_webhooks.py
@@ -5,10 +5,13 @@ import time
 from discord import SyncWebhook
 
 from coords_scraper import check_for_coords
+from server_status import query_server
 
 POLLING_INTERVAL = int(os.getenv("POLLING_INTERVAL", 30))
 COORD_WEBHOOK_URL = os.environ["COORD_WEBHOOK_URL"]
 STATUS_WEBHOOK_URL = os.environ["STATUS_WEBHOOK_URL"]
+
+STATUS_MESSAGE_ID = int(os.getenv("STATUS_MESSAGE_ID") or 0)
 
 
 def send_coords(log_folder: str):
@@ -19,6 +22,23 @@ def send_coords(log_folder: str):
         webhook.send(embed=embed)
 
 
+def update_server_status(message_id: int):
+    server = query_server()
+    webhook = SyncWebhook.from_url(STATUS_WEBHOOK_URL)
+    embed = server.to_embed()
+
+    # First-time setup.
+    if not message_id:
+        message = webhook.send(embed=embed, wait=True)
+        raise UserWarning(
+            f"FIRST-TIME SETUP: The message has been sent with the ID {message.id}. "
+            f"Set the environment variable `STATUS_MESSAGE_ID` to this value and re-run this script."
+        )
+
+    message = webhook.fetch_message(message_id)
+    message.edit(embed=embed)
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         raise ValueError("No log folder provided.")
@@ -27,4 +47,6 @@ if __name__ == "__main__":
 
     while True:
         send_coords(sys.argv[1])
+        update_server_status(STATUS_MESSAGE_ID)
+
         time.sleep(POLLING_INTERVAL)
