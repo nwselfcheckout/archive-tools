e11c0200a53c52da0751936fdc64685befdbbe2b
mosguinz <mos.guinz@gmail.com>
2024-08-23 14:18:07 -0700

    Convert Minecraft MOTD formatting to Markdown

--- webhooks/server_status.py
+++ webhooks/server_status.py
@@ -2,6 +2,8 @@ import os
 from dataclasses import dataclass
 
 from mcstatus import JavaServer
+from mcstatus import motd
+from mcstatus.motd.components import ParsedMotdComponent
 
 SERVER_ADDRESS = os.environ["SERVER_ADDRESS"]
 
@@ -16,6 +18,37 @@ class ServerInfo:
     version: str = None
     motd: str = None
 
+
+# Supported MOTD-to-Markdown formatters.
+markdown_formatters = {
+    motd.Formatting.ITALIC: "*",
+    motd.Formatting.BOLD: "**",
+    motd.Formatting.UNDERLINED: "__",
+    motd.Formatting.STRIKETHROUGH: "~~",
+}
+
+
+def to_markdown(parsed: list[ParsedMotdComponent]) -> str:
+    """Convert Minecraft MOTD components into Markdown formatted string."""
+    md_text = []
+    last_formatter = None
+
+    for component in parsed:
+        if type(component) is str:
+            md_text.append(component)
+            continue
+        if formatter := markdown_formatters.get(component):
+            md_text.append(formatter)
+            last_formatter = component
+            continue
+        if component is motd.Formatting.RESET:
+            md_text.append(markdown_formatters.get(last_formatter, ""))
+            last_formatter = None
+            continue
+    if last_formatter:
+        md_text.append(markdown_formatters.get(last_formatter, ""))
+
+    return "".join(md_text)
 
 
 def query_server() -> ServerInfo:
