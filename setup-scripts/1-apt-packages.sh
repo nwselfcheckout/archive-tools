77c8dec300dea3af895058079f0ef59cf652ac08
mosguinz <mos.guinz@gmail.com>
2024-08-23 23:31:22 -0700

    Add python to apt-packages for first-time setup

--- setup-scripts/1-apt-packages.sh
+++ setup-scripts/1-apt-packages.sh
@@ -3,6 +3,11 @@
 sudo apt update
 sudo apt install screen openjdk-21-jre firewalld pip zip unzip
 
+# Get Python 3.10 for other scripts
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.10-full
+
 echo Checking to see if everything\'s installed...
 
 screen --version
